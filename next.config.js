3f8ae801ff44e1f79dd52ff656c786f1383db866
Team <team@fastcode.com.br>
2022-05-24 20:00:20 -0300

    update api request

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/v1/:path*',
+        destination: 'http://gateway.marvel.com/v1/public/comics/323/events/:path*',
+      },
+    ]
+  },
   compiler: {
     styledComponents: true
   }
