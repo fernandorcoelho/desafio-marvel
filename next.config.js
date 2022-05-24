922ad07e7c5e09d135973f631573ef477ecfeb55
Team <team@fastcode.com.br>
2022-05-24 20:08:02 -0300

    update api request

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/v1/:path*',
-        destination: 'http://gateway.marvel.com/v1/public/comics/323/events/:path*',
-      },
-    ]
-  },
   compiler: {
-    styledComponents: truepp
+    styledComponents: true
   }
 };
--- src/services/client/index.ts
+++ src/services/client/index.ts
@@ -2,16 +2,15 @@ import axios from 'axios';
 import md5 from 'md5';
 
 export function setupAPI() {
-  const publicKey = '7c9d0981ca0b182a60d9bab410e81c4f';
   const privateKey = 'bda1a3344a65c93ffa8aa22355c283048c9dfc8b';
   const ts = Number(new Date());
-  const hash = md5(ts + privateKey + publicKey);
+  const hash = md5(ts + privateKey + process.env.PUBLIC_KEY);
 
   const api = axios.create({
     baseURL: 'https://gateway.marvel.com/v1/public/',
     params: {
       ts,
-      apikey: publicKey,
+      apikey: process.env.PUBLIC_KEY,
       hash
     },
     // headers: {
