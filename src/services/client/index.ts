5bf450b9867bdc111a9158d262c596aa9e78f011
Team <team@fastcode.com.br>
2022-05-24 19:51:59 -0300

    update api request

--- src/services/client/index.ts
+++ src/services/client/index.ts
@@ -15,6 +15,8 @@ export function setupAPI() {
       hash
     },
     headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      "Access-Control-Allow-Origin": "*",
       'Accept': '/'
     }
   });
